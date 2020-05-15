616d24a971fbe343543e0af03f623c490da9cd63
reus <reusee@gmail.com>
2020-05-15 22:29:55 +0800

    update

--- main.go
+++ main.go
@@ -26,14 +26,23 @@ func main() {
 	ctxDialer := dialer.(proxy.ContextDialer)
 	client := &http.Client{
 		Transport: &http.Transport{
-			DialContext: ctxDialer.DialContext,
-			Dial:        dialer.Dial,
+			DialContext:           ctxDialer.DialContext,
+			Dial:                  dialer.Dial,
+			ResponseHeaderTimeout: time.Second * 32,
 		},
-		Timeout: time.Second * 32,
 	}
 
+	retry := 10
+do:
+
 	resp, err := client.Get(addr)
-	ce(err, "get %s", addr)
+	if err != nil {
+		if retry > 0 {
+			retry--
+			goto do
+		}
+		ce(err, "get %s", addr)
+	}
 	defer resp.Body.Close()
 
 	var filename string
@@ -52,16 +61,30 @@ func main() {
 		filename = filepath.Base(u.Path)
 	}
 
-	if _, err := os.Stat(filename); err == nil {
-		panic(me(nil, "file exists: %s", filename))
-	}
+	//if _, err := os.Stat(filename); err == nil {
+	//	panic(me(nil, "file exists: %s", filename))
+	//}
 
 	tmpFilename := filename + ".tmp"
 	f, err := os.Create(tmpFilename)
 	ce(err, "create %s", tmpFilename)
 
-	_, err = io.Copy(f, resp.Body)
-	ce(err)
+	buf := make([]byte, 8192)
+	c := 0
+	for {
+		n, err := resp.Body.Read(buf)
+		if n > 0 {
+			_, err := f.Write(buf[:n])
+			ce(err)
+		}
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			ce(err)
+		}
+		c += n
+		pt("%s %d\n", addr, c)
+	}
 
 	ce(f.Close())
 	ce(os.Rename(tmpFilename, filename))
